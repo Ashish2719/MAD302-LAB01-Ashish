ffcfd5d58751ca954a0d23b4bbfb0cb3b6bc4890
ashishprajapati <ashishprajapati27999@gmail.com>
2026-02-15 14:03:24 -0500

    Add Profile data clas

---
+++ app/src/main/java/com/example/profilelistapp/Profile.kt
@@ -0,0 +1,17 @@
+package com.example.profilelistapp
+
+/**
+ * Course: MAD302
+ * Lab: LAB 1
+ * Name: YOUR FULL NAME
+ * Student ID: YOUR STUDENT ID
+ * Date: YYYY-MM-DD
+ *
+ * Description:
+ * This data class represents a user profile containing
+ * a name and an age.
+ */
+data class Profile(
+    val name: String,
+    val age: Int
+)
